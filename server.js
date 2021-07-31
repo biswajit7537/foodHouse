31b92a6fe6c2958fdd608f4bc9b1aea1802715bd
Biswajit Mishra <biswajitmishra603@gmail.com>
2021-07-31 12:21:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.render('home');
+})
+
+// set template engine
+
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'));
+app.set('view engine','ejs');
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server started on the port ${PORT}`);
+});
