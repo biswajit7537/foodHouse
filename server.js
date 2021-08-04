e5a4051f9e8a426a735ce83dab9ab9c80fce835b
Biswajit Mishra <biswajitmishra603@gmail.com>
2021-08-04 20:42:51 +0530

    creat login and register page and cart page

--- server.js
+++ server.js
@@ -8,15 +8,29 @@ const PORT = process.env.PORT || 3000;
 // assets
 app.use(express.static('public'));
 
-app.get("/",(req,res)=>{
-    res.render('home');
-})
 
 // set template engine
 
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+
+
+app.get("/",(req,res)=>{
+    res.render('home');
+});
+
+app.get("/cart",(req,res)=>{
+    res.render('customers/cart');
+});
+
+app.get("/login",(req,res)=>{
+    res.render('auth/login');
+});
+
+app.get("/register",(req,res)=>{
+    res.render('auth/register');
+});
 
 
 
